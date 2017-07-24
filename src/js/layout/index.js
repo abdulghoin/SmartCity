3d665f8b77ccc420025cfea6ac709e34c7f827a5
abdulghoin <abdulghoin@gmail.com>
2017-07-25 03:31:43 +0700

    starter

---
+++ src/js/layout/index.js
@@ -0,0 +1,12 @@
+// import Packages
+import React from 'react'
+
+const LayOut =({children})=>{
+  return(
+    <section id='LayOut'>
+      {children}
+    </section>
+  )
+}
+
+export default LayOut
---
+++ src/js/pages/Home.js
@@ -0,0 +1,14 @@
+// import Packages
+import React from 'react'
+import Helmet from 'react-helmet'
+
+const Home = ()=>{
+  return(
+    <section id='home'>
+      <Helmet title='Home' />
+
+    </section>
+  )
+}
+
+export default Home
