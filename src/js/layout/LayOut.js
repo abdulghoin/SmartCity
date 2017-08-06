d7ecaaf89d7d054cf7a65ee50a48cb49d40a5bf9
Abdul Ghofur <abdulghoin@gmail.com>
2017-08-06 18:52:27 +0700

    no message

---
+++ src/js/layout/LayOut.js
@@ -0,0 +1,19 @@
+// import Packages
+import React from 'react'
+
+// import Components
+// import Header from './Header'
+import Header from 'Header'
+
+const LayOut =({children})=>{
+  return(
+    <section id='LayOut'>
+      <Header />
+      <div class='container'>
+        {children}
+      </div>
+    </section>
+  )
+}
+
+export default LayOut
